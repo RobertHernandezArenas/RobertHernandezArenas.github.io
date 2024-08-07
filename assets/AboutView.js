d7334d86b912a60ff671c6dee6ab8ef2a29581b8
Robert Hernandez Arenas <airbusjayrobert@gmail.com>
2024-08-07 17:31:39 +0200

    demo menu vue

---
+++ assets/AboutView.js
@@ -0,0 +1,2 @@
+import{_ as e}from"./index.js";import{g as o,o as t,k as s}from"./vue.js";const c={},a={class:"about"},n=s("h1",null,"This is an about page",-1),_=[n];function r(i,d){return t(),o("div",a,_)}const u=e(c,[["render",r]]);export{u as default};
+//# sourceMappingURL=AboutView.js.map
