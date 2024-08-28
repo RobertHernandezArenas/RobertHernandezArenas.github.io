06b9068dd54181ceae268965017ced3ae2c7b30e
Robert Hernandez Arenas <airbusjayrobert@gmail.com>
2024-08-28 20:37:16 +0200

    Portfolio demo

--- assets/ContactView.js
+++
@@ -1,2 +0,0 @@
-import{_ as t}from"./index.js";import{q as e,l as c,y as o}from"./vue.js";const s={},n={class:"contact"},a=o("h1",null,"This is an contact page",-1),_=[a];function r(i,d){return c(),e("div",n,_)}const h=t(s,[["render",r]]);export{h as default};
-//# sourceMappingURL=ContactView.js.map
